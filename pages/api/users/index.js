4286f5b8d14f15bd72aae05a23d5166a4298e570
kajal-2011 <kajalrekha888@gmail.com>
2025-11-02 19:31:53 +0600

    added authentication system with register, login functionality

---
+++ pages/api/users/index.js
@@ -0,0 +1,19 @@
+import { createUser, getAllUsers } from "@/controllers/userController";
+import { connectDB } from "@/lib/db";
+
+
+export default async function handler(req, res) {
+    await connectDB();
+
+    if (req.method === "POST") {
+        console.log("hello");
+        return createUser(req, res);
+    } else if (req.method === "GET") {
+        return getAllUsers(req, res);
+    } else {
+        res.setHeader("Allow", ["GET", "POST"]);
+        return res
+            .status(405)
+            .json({ error: `Method ${req.method} not allowed` });
+    }
+}
